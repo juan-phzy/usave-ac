ad354f09accbae2967c470a95bef2f7247abbde3
Juan Hernandez <jp.hernandez.yz@gmail.com>
2024-09-11 19:46:35 -0400

    mobile home page finished

--- components/pages/Hero.tsx
+++ components/pages/Hero.tsx
@@ -6,14 +6,24 @@ const Hero = () => {
         <Image
           className='object-cover'
           src='/images/main-bg.jpg'
-          alt='hero-image'
+          alt='hero-bg'
           fill
         />
         <div className="white-overlay"></div>
       </div>
 
       <div className="hero-banner">
-
+        <div className="banner-img">
+          <Image
+            className='object-contain'
+            src='/images/vectorguy.png'
+            alt='hero-image'
+            fill
+          />
+        </div>
+        <div className="banner-title">Installation, Repair & Maintenance </div>
+        <div className="banner-desc">On ALL brands of heating, air conditioning, and refrigeration systems.</div>
+        <button className="btn-light"><a href="/">Request Service</a></button>
       </div>
     </section>
   )
